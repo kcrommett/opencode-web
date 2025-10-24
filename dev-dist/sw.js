9a364adbac73e6551e1faaa4a346a9e6622ef350
Kyle Crommett <kyle@latitudes.io>
2025-10-23 22:15:56 -0700

    fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.0aennvbiohs"
+    "revision": "0.vjomns7r9ps"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
